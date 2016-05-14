4949081beaac4ba7bcca5a39f662ea3a92e486d3
Jemish Patel <jpatel@pivotal.io>
2016-05-14 01:45:46 -0700

    fixed loginc for hdb start script

--- hdb2/centos7/start-hdb.sh
+++ hdb2/centos7/start-hdb.sh
@@ -8,10 +8,11 @@ fi
 
 
 if [ "${NAMENODE}" == "${HOSTNAME}" ]; then
+ echo 'sleeping ' > /home/gpadmin/start_hdb.log
  sleep 20
  if [ "`sudo -u hdfs hdfs dfsadmin -report | grep Live | awk '{print $3}' | tr -d "(|)" | tr -d ":"`" == 3 ]; then
    #  start Hawq as we are on Master
-   echo "hdfs is alive starting HAWQ"
+   echo "hdfs is alive and starting HAWQ on ${HOSTNAME} " >> /home/gpadmin/start_hdb.log
    if [ ! -d /home/gpadmin/hawq-data-directory/masterdd ]; then
      sed 's|localhost|centos7-namenode|g' -i /data/hdb2/etc/hawq-site.xml
      echo 'centos7-datanode1' > /data/hdb2/etc/slaves
@@ -23,7 +24,11 @@ if [ "${NAMENODE}" == "${HOSTNAME}" ]; then
    fi
  
    if [ -d /home/gpadmin/hawq-data-directory/masterdd ]; then
+     echo "masterdd exists" >> /home/gpadmin/start_hdb.log
      if [ -z "`ps aux | grep masterdd | grep -v grep `" ]; then
+       echo "HDB already running " >> /home/gpadmin/start_hdb.log
+     else
+       echo "starting HDB processes" >> /home/gpadmin/start_hdb.log
        source /data/hdb2/greenplum_path.sh
        hawq start cluster -a
      fi
