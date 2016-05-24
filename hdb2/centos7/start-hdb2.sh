f8f60cce1aede51976141c74cc0c3dadaa1dc34f
Jemish Patel <jpatel@pivotal.io>
2016-05-24 09:47:51 -0700

    better flow

--- hdb2/centos7/start-hdb2.sh
+++ hdb2/centos7/start-hdb2.sh
@@ -2,6 +2,63 @@
 
 sudo /usr/sbin/sshd
 
+
+config_pxf () {
+  sudo sed -i 's|/usr/lib/hadoop/lib/native|/usr/hdp/current/hadoop-client/lib/native|' /etc/pxf/conf/pxf-env.sh
+  sudo sed -i 's|/usr/java/default|/etc/alternatives/java_sdk|' /etc/pxf/conf/pxf-env.sh
+  echo "# Hadoop home directory" >> /etc/pxf/conf/pxf-env.sh
+  echo "export HADOOP_HOME=${HADOOP_HOME:-/usr/hdp/current/hadoop-client}" >> /etc/pxf/conf/pxf-env.sh
+}
+
+init_pxf () {
+  sudo service pxf-service init
+}
+
+start_pxf () {
+    sudo service pxf-service start
+}
+
+config_slaves () {
+  sed 's|localhost|centos7-namenode|g' -i /data/hdb2/etc/hawq-site.xml
+  echo 'centos7-datanode1' > /data/hdb2/etc/slaves
+  echo 'centos7-datanode2' >> /data/hdb2/etc/slaves
+  echo 'centos7-datanode3' >> /data/hdb2/etc/slaves
+  echo "edited the slaves file " >> /home/gpadmin/start_hdb.log
+}
+
+config_access () {
+  echo "host  all     gpadmin    0.0.0.0/0       trust" >> /home/gpadmin/hawq-data-directory/masterdd/pg_hba.conf
+}
+
+config_guc () {
+  source /data/hdb2/greenplum_path.sh
+  hawq config -c optimizer_analyze_root_partition -v on
+  hawq config -c optimizer -v on
+}
+
+init_hawq () {
+  source /data/hdb2/greenplum_path.sh
+  hawq init cluster -a
+}
+
+inst_madlib () {
+  source /data/hdb2/greenplum_path.sh
+  gppkg -i /data/madlib-ossv1.9_pv1.9.5_hawq2.0-rhel5-x86_64.tar.gz -d /home/gpadmin/hawq-data-directory/masterdd
+  /usr/local/madlib/bin/madpack –p hawq install
+}
+
+start_hawq () {
+  source /data/hdb2/greenplum_path.sh
+  hawq start cluster -a
+}
+
+restart_hawq () {
+   source /data/hdb2/greenplum_path.sh
+   hawq restart cluster -a
+}
+
+
+
 if [ -f /etc/profile.d/hadoop.sh ]; then
   . /etc/profile.d/hadoop.sh
 fi
@@ -58,59 +115,3 @@ else
   config_pxf
   start_pxf
 fi
-
-
-## PXF Config Function
-config_pxf () {
-  sudo sed -i 's|/usr/lib/hadoop/lib/native|/usr/hdp/current/hadoop-client/lib/native|' /etc/pxf/conf/pxf-env.sh
-  sudo sed -i 's|/usr/java/default|/etc/alternatives/java_sdk|' /etc/pxf/conf/pxf-env.sh
-  echo "# Hadoop home directory" >> /etc/pxf/conf/pxf-env.sh
-  echo "export HADOOP_HOME=${HADOOP_HOME:-/usr/hdp/current/hadoop-client}" >> /etc/pxf/conf/pxf-env.sh
-}
-
-init_pxf () {
-  sudo service pxf-service init
-}
-
-start_pxf () {
-    sudo service pxf-service start
-}
-
-config_slaves () {
-  sed 's|localhost|centos7-namenode|g' -i /data/hdb2/etc/hawq-site.xml
-  echo 'centos7-datanode1' > /data/hdb2/etc/slaves
-  echo 'centos7-datanode2' >> /data/hdb2/etc/slaves
-  echo 'centos7-datanode3' >> /data/hdb2/etc/slaves
-  echo "edited the slaves file " >> /home/gpadmin/start_hdb.log
-}
-
-config_access () {
-  echo "host  all     gpadmin    0.0.0.0/0       trust" >> /home/gpadmin/hawq-data-directory/masterdd/pg_hba.conf
-}
-
-config_guc () {
-  source /data/hdb2/greenplum_path.sh
-  hawq config -c optimizer_analyze_root_partition -v on
-  hawq config -c optimizer -v on
-}
-
-init_hawq () {
-  source /data/hdb2/greenplum_path.sh
-  hawq init cluster -a
-}
-
-inst_madlib () {
-  source /data/hdb2/greenplum_path.sh
-  gppkg -i /data/madlib-ossv1.9_pv1.9.5_hawq2.0-rhel5-x86_64.tar.gz -d /home/gpadmin/hawq-data-directory/masterdd
-  /usr/local/madlib/bin/madpack –p hawq install
-}
-
-start_hawq () {
-  source /data/hdb2/greenplum_path.sh
-  hawq start cluster -a
-}
-
-restart_hawq () {
-   source /data/hdb2/greenplum_path.sh
-   hawq restart cluster -a
-}
