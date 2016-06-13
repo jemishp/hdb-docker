9f108dc432878c91973de92c4f603e333c575203
jpatel-pivotal <jpatel@pivotal.io>
2016-06-13 14:51:00 -0700

    Fixing fsdefaultFS in core-site

--- hdb2/centos7/start-hdfs.sh
+++ hdb2/centos7/start-hdfs.sh
@@ -8,15 +8,18 @@ fi
 
 if [ "${NAMENODE}" == "${HOSTNAME}" ]; then
   if [ ! -d /tmp/hdfs/name/current ]; then
+    sudo sed -i 's/\${hdfs.namenode}/\${NAMENODE}/g' /etc/hadoop/conf/core-site.xml
     su -l hdfs -c "hdfs namenode -format"
   fi
   
   if [ -z "`ps aux | grep org.apache.hadoop.hdfs.server.namenode.NameNode | grep -v grep`" ]; then
+    sudo sed -i 's/\${hdfs.namenode}/\${NAMENODE}/g' /etc/hadoop/conf/core-site.xml
     su -l hdfs -c "hadoop-daemon.sh start namenode"
     sudo -u hdfs hdfs dfs -chmod 777 /
   fi
 else
   if [ -z "`ps aux | grep org.apache.hadoop.hdfs.server.datanode.DataNode | grep -v grep`" ]; then
+    sudo sed -i 's/\${hdfs.namenode}/\${NAMENODE}/g' /etc/hadoop/conf/core-site.xml
     su -l hdfs -c "hadoop-daemon.sh start datanode"
   fi
 fi
