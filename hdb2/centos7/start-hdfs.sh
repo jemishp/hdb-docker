81dcc66b9318ee3b05bef71f896dd79ff41ef098
jpatel@pivotal.io <jpatel@pivotal.io>
2016-08-23 12:03:22 -0700

    starting scdf admin

--- hdb2/centos7/start-hdfs.sh
+++ hdb2/centos7/start-hdfs.sh
@@ -12,7 +12,7 @@ if [ "${NAMENODE}" == "${HOSTNAME}" ]; then
     sudo sed -i "s/\${user.name}/${USER}/g" /etc/hadoop/conf/hdfs-site.xml
     su -l hdfs -c "hdfs namenode -format"
   fi
-  
+
   if [ -z "`ps aux | grep org.apache.hadoop.hdfs.server.namenode.NameNode | grep -v grep`" ]; then
     sudo sed -i "s/\${hdfs.namenode}/${NAMENODE}/g" /etc/hadoop/conf/core-site.xml
     sudo sed -i "s/\${user.name}/${USER}/g" /etc/hadoop/conf/hdfs-site.xml
@@ -28,4 +28,4 @@ else
 fi
 
 su -l gpadmin -c "start-hdb.sh"
-
+su -l gpadmin -c "start-scdf.sh"
