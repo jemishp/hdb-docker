2a03f69d1b17148192db796a18bda71dc9e83158
jpatel@pivotal.io <jpatel@pivotal.io>
2016-09-21 23:05:20 -0700

    addning flush

--- hdb2/centos7/start-scdf.sh
+++ hdb2/centos7/start-scdf.sh
@@ -15,7 +15,7 @@ start_scdf_shell () {
   cd /data/
   echo -e 'app import --uri http://bit.ly/stream-applications-kafka-maven' >> demo.cmd
   echo -e 'stream destroy --name demo ' >> demo.cmd
-  echo -e 'stream create --name demo --definition "http --server.port=9000 | hdfs --hdfs.fs-uri=hdfs://centos7-namenode --hdfs.directory=/demo --hdfs.file-name=demoData --hdfs.in-use-prefix=IU" --deploy' >> demo.cmd
+  echo -e 'stream create --name demo --definition "http --server.port=9000 | hdfs --hdfs.fs-uri=hdfs://centos7-namenode --hdfs.directory=/demo --hdfs.file-name=demoData --hdfs.in-use-prefix=IU --hdfs.flush-timeout=1000" --deploy' >> demo.cmd
   echo 'script --file demo.cmd' | java -jar spring-cloud-dataflow-shell-*.BUILD-SNAPSHOT.jar
 
 }
