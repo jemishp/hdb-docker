a9eeac6614b016eb8c1f1d5688440b569a9cbfef
jpatel@pivotal.io <jpatel@pivotal.io>
2016-10-25 13:54:32 -0700

    refactor stream definition

--- hdb2/centos7/start-scdf.sh
+++ hdb2/centos7/start-scdf.sh
@@ -26,7 +26,8 @@ start_scdf_shell () {
   echo -e 'app import --uri http://bit.ly/stream-applications-kafka-maven' >> demo.cmd
   echo -e 'stream all destroy --force ' >> demo.cmd
   echo -e 'stream create --definition "http --server.port=9000 | scriptable-transform --script=""import groovy.json.JsonSlurper\ndef jsonCar = new JsonSlurper().parseText( payload )\ndef csvString = jsonCar.amountOfFuel + "","" + jsonCar.tankCapacity + "","" + jsonCar.currentMileage + "","" + jsonCar.currentGear + "","" + jsonCar.currentSpeed + "","" + jsonCar.currentRpm + "","" + jsonCar.eBrake + "","" + jsonCar.engTemp + "","" + jsonCar.insideTemp + "","" + jsonCar.outsideTemp + "","" + jsonCar.frontLeftPsi + "","" + jsonCar.frontRightPsi + "","" +jsonCar.rearLeftPsi + "","" + jsonCar.rearRightPsi + "","" + jsonCar.make + "","" + jsonCar.model + "","" + jsonCar.vin + "","" + jsonCar.year\nreturn csvString"" --language=groovy | hdfs --hdfs.fs-uri=hdfs://centos7-namenode:8020 --hdfs.directory=/demo --hdfs.file-name=demoData --hdfs.rollover=10000" --name demo --deploy' >> demo.cmd
-  echo 'script --file demo.cmd' | java -jar spring-cloud-dataflow-shell-*.BUILD-SNAPSHOT.jar
+  echo -e 'stream create --definition "http --server.port=9000  | hdfs --hdfs.fs-uri=hdfs://centos7-namenode:8020 --hdfs.directory=/demo --hdfs.file-name=demoData --hdfs.rollover=10000" --name demo --deploy' >> demo2.cmd
+  echo 'script --file demo2.cmd' | java -jar spring-cloud-dataflow-shell-*.BUILD-SNAPSHOT.jar
 
 
 
